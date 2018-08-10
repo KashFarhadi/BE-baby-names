f084847cd60270b8b82053f347c4f9049da3e2ce
Piero Madar <pmadar@tycoint.com>
2018-08-09 21:21:38 -0400

    outline for babynames tester

---
+++ test_babynames.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+from babynames import main, extract_names
+
+class TestBabynames(unittest.TestCase):
+    def test_extract_names(self):
+        pass
