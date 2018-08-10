9c53db74ea1109298b4df6c1ac2a639869ab58af
Piero Madar <pmadar@tycoint.com>
2018-08-09 20:16:22 -0400

    The REAL argparse and autopep8 commit!

--- babynames.py
+++ babynames.py
@@ -1,4 +1,8 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# BabyNames python coding exercise.
+
 # Copyright 2010 Google Inc.
 # Licensed under the Apache License, Version 2.0
 # http://www.apache.org/licenses/LICENSE-2.0
@@ -8,9 +12,9 @@
 
 import sys
 import re
+import argparse
 
-"""Baby Names exercise
-
+"""
 Define the extract_names() function below and change main()
 to call it.
 
@@ -34,35 +38,40 @@ Suggested milestones for incremental development:
  -Fix main() to use the extract_names list
 """
 
+
 def extract_names(filename):
-  """
-  Given a file name for baby.html, returns a list starting with the year string
-  followed by the name-rank strings in alphabetical order.
-  ['2006', 'Aaliyah 91', Aaron 57', 'Abagail 895', ' ...]
-  """
-  # +++your code here+++
-  return
+    """
+    Given a file name for baby.html, returns a list starting with the year string
+    followed by the name-rank strings in alphabetical order.
+    ['2006', 'Aaliyah 91', Aaron 57', 'Abagail 895', ' ...]
+    """
+    # +++your code here+++
+    return
 
 
 def main():
-  # This command-line parsing code is provided.
-  # Make a list of command line arguments, omitting the [0] element
-  # which is the script itself.
-  args = sys.argv[1:]
+    # This command-line parsing code is provided.
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--summaryfile', help='creates a summary file', action='store_true')
+    # The nargs option instructs the parser to expect 1 or more filenames.
+    # It will also expand wildcards just like the shell, e.g. 'baby*.html' will work.
+    parser.add_argument('files', help='filename(s) to parse', nargs='+')
+    args = parser.parse_args()
 
-  if not args:
-    print 'usage: [--summaryfile] file [file ...]'
-    sys.exit(1)
+    if not args:
+        parser.print_usage()
+        sys.exit(1)
 
-  # Notice the summary flag and remove it from args if it is present.
-  summary = False
-  if args[0] == '--summaryfile':
-    summary = True
-    del args[0]
+    file_list = args.files
 
-  # +++your code here+++
-  # For each filename, get the names, then either print the text output
-  # or write it to a summary file
-  
+    # option flag
+    create_summary = args.summaryfile
+
+    # +++your code here+++
+    # For each filename, get the names, then either print the text output
+    # or write it to a summary file
+
+
 if __name__ == '__main__':
-  main()
+    main()
